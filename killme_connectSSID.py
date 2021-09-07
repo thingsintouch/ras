bc66d369f3de68fee7ce575e9fa2957109604f52
lubusax <lu.bu.sax@gmail.com>
2021-09-07 22:25:19 +0200

    test - process SSID

---
+++ killme_connectSSID.py
@@ -0,0 +1,14 @@
+from multiprocessing import Process
+from bluetooth import connect_To_SSID
+
+ssidName = 'FRITZ!Box 6490 Cable'
+ssidPassword = '522968262011056618'
+print("#"*100)
+print("#"*100)
+print("#"*100)
+print(f'ssidName : {ssidName}; ssidPassword : {ssidPassword};')
+connectToSSIDProcess = Process(target=connect_To_SSID.main, args=(ssidName, ssidPassword, ))
+connectToSSIDProcess.start()
+print("#"*100)
+print("#"*100)
+print("#"*100)
