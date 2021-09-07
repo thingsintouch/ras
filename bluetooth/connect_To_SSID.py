b4f56c4649878b17270e8f31ed51bd390867ea89
lubusax <lu.bu.sax@gmail.com>
2021-09-07 22:39:32 +0200

    display again

--- bluetooth/connect_To_SSID.py
+++ bluetooth/connect_To_SSID.py
@@ -1,30 +1,30 @@
-#import time
+import time
 
 from common import constants as co
-#from common.logger import loggerINFO, loggerCRITICAL, loggerDEBUG, loggerERROR
-#from common.params import Params
-#from display.helpers import Oled
+from common.logger import loggerINFO, loggerCRITICAL, loggerDEBUG, loggerERROR
+from common.params import Params
+from display.helpers import Oled
 
 from common.common import runShellCommand_and_returnOutput as rs
-#from common.connectivity import internetReachable
+from common.connectivity import internetReachable
 
-#params = Params(db=co.PARAMS)
+params = Params(db=co.PARAMS)
 
 
 def main(ssidName, ssidPassword):
-    #oled = Oled()
-    #params.put("displayClock", "no")
-    #text = f"CONNECTING\nWITH SSID\n{ssidName}"
-    #loggerINFO(text)
-    #oled.three_lines_text(text)
+    oled = Oled()
+    params.put("displayClock", "no")
+    text = f"CONNECTING\nWITH SSID\n{ssidName}"
+    loggerINFO(text)
+    oled.three_lines_text(text)
     answer = (rs('sudo nmcli dev wifi con '+ssidName+' password '+ssidPassword))
-    #if internetReachable():
-    #    text = f"CONNECTED\nWITH THE\nINTERNET"
-    #else:
-    #    text = f"NO CONNECTION\nWITH THE\nINTERNET"
-    #loggerINFO(text)
-    #time.sleep(4) 
-    #params.put("displayClock", "yes")
+    if internetReachable():
+       text = f"CONNECTED\nWITH THE\nINTERNET"
+    else:
+       text = f"NO CONNECTION\nWITH THE\nINTERNET"
+    loggerINFO(text)
+    time.sleep(4) 
+    params.put("displayClock", "yes")
 
 if __name__ == "__main__":
     main()
