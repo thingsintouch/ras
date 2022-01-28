4b6c3c57a54817c6109e3ab18b2f83e1e23a3004
root <root@raspberrypi>
2022-01-28 12:09:40 -0500

    add custom params

---
+++ factory_settings/custom_params.py
@@ -0,0 +1,21 @@
+factory_settings = {
+    "firmwareAtShipment"  : "v2.6 - LOCAL",
+    "productName"         : "RAS2.1",
+    "productionDate"      : "20211025",
+    "productionLocation"  : "Frankfurt",
+    "productionNumber"    : "01",
+    "qualityInspector"    : "Luis",
+    "setup_password"      : "HDRTFG",
+    "ordered_by"          : "Vincent Young",
+    "invoice_nr"          : "free of charge",
+    "tz"                  : "America/New_York",
+    "hardware_machine"    : "RPi Zero W",
+    "hardware_card_reader": "MFRC522",
+    "hardware_display"    : "oled sh1106",
+    "hardware_sound"      : "passive buzzer",
+    "time_format"         : "12 hour",
+    "odooUrlTemplate"     : "https://pri.forgeflow.io:443",
+    "odoo_host"           : "pri.forgeflow.io",
+    "odoo_port"           : "443"
+}
+
