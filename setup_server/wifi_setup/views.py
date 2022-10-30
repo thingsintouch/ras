56d3d39bf211ced3b33d18d327806a05442b58f6
lubusax <lu.bu.sax@gmail.com>
2022-10-30 05:47:31 -0400

    wifi at the beginning

--- setup_server/wifi_setup/views.py
+++ setup_server/wifi_setup/views.py
@@ -2,7 +2,7 @@ from flask import render_template,url_for,flash, redirect,request,Blueprint
 from flask_login import current_user,login_required
 from setup_server.wifi_setup.forms import WiFiForm, WiFiSetupResult
 from common.common import runShellCommand_and_returnOutput as rs
-from common.connect_To_SSID import main as connect_to_wifi_network
+from common.connect_To_SSID import store_wifi_network_and_password #main as connect_to_wifi_network
 from common.counter_ops import reset_counter
 
 wifi_setup = Blueprint('wifi_setup',__name__)
@@ -12,11 +12,12 @@ wifi_setup = Blueprint('wifi_setup',__name__)
 def define_wifi():
     form = WiFiForm()
     if form.validate_on_submit():
+        reset_counter("counter_wifi_connected")
         reset_counter("counter_wifi_disconnected")
         wifi_network = form.wifi_network.data
         wifi_password = form.password.data
-        if connect_to_wifi_network(wifi_network, wifi_password):
-            result = 1
+        if store_wifi_network_and_password(wifi_network, wifi_password):
+            result = 1 
         else:
             result = 0
         return redirect(url_for('wifi_setup.wifi_setup_result', result=result ))
