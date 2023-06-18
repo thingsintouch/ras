c04da95bea250b2fccccf4dc2adce364a2536021
lubusax <lu.bu.sax@gmail.com>
2023-06-18 12:17:34 +0200

    other values

--- odoo/registerClockings.py
+++ odoo/registerClockings.py
@@ -54,7 +54,7 @@ def registerClockings():
                     card_code_and_timestamp = clocking_tuple[2]
                     timestamp = clocking_tuple[0]
                     answer = register_async_clocking(card_code, timestamp)
-                    time.sleep(2.5)
+                    time.sleep(1.8)
                 except Exception as e:
                     loggerDEBUG(f"Could not Register Clocking {card_code_and_timestamp} - Exception: {e}")
                     answer = False
