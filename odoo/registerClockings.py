eec0795f7a186c8272d3e0116d48bb2a39558867
lubusax <lu.bu.sax@gmail.com>
2023-06-18 13:35:24 +0200

    new values

--- odoo/registerClockings.py
+++ odoo/registerClockings.py
@@ -54,7 +54,7 @@ def registerClockings():
                     card_code_and_timestamp = clocking_tuple[2]
                     timestamp = clocking_tuple[0]
                     answer = register_async_clocking(card_code, timestamp)
-                    time.sleep(2.7)
+                    time.sleep(2.6)
                 except Exception as e:
                     loggerDEBUG(f"Could not Register Clocking {card_code_and_timestamp} - Exception: {e}")
                     answer = False
--- odooRegisterClockings/manager.py
+++ odooRegisterClockings/manager.py
@@ -15,7 +15,7 @@ def main():
         registerClockings()
         minimum = get_period("period_register_clockings")
         clockings_waiting = len(listdir(CLOCKINGS))
-        period_register_clockings = max(minimum, clockings_waiting*3)
+        period_register_clockings = max(minimum, clockings_waiting*4)
         sleep(period_register_clockings)
 
 
