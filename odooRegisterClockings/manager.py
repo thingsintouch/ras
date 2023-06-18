d4b88a61f5358228d8765703c4ea76b6ddd949f1
lubusax <lu.bu.sax@gmail.com>
2023-06-18 03:02:39 +0200

    set period for register clockings large enough by counting the clockings to be sent

--- odooRegisterClockings/manager.py
+++ odooRegisterClockings/manager.py
@@ -1,9 +1,11 @@
 from time import sleep
+from os import listdir
 
 from odoo.registerClockings import registerClockings
 
 from common.common import get_period
 from common.common import BLOCKING_waiting_until_RAS_acknowledged_from_Odoo
+from common.constants import CLOCKINGS
 
 def main():
 
@@ -11,7 +13,10 @@ def main():
 
     while True:
         registerClockings()
-        sleep(get_period("period_register_clockings"))
+        minimum = get_period("period_register_clockings")
+        clockings_waiting = len(listdir(CLOCKINGS))
+        period_register_clockings = max(minimum, clockings_waiting)
+        sleep(period_register_clockings)
 
 
 if __name__ == "__main__":
