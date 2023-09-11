5348e15abd6b171393de6a86f669f0ac2db80a7b
lubusax <lu.bu.sax@gmail.com>
2023-09-11 20:06:49 +0200

    auto

--- monitoring/all_python_processes.sh
+++ monitoring/all_python_processes.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# List all python3 subprocesses without full path
+# List all python3 processes and subprocesses
 pgrep -a python3 | while read pid cmd; do
-  # Check if the process is still running
-  if kill -0 $pid 2>/dev/null; then
-    # Remove the path from the command name using cut
-    cmd=$(echo "$cmd" | cut -d' ' -f1 | cut -d'/' -f5)
-    # Call pstree with the command name
-    pstree -a -p $pid "$cmd"
-  fi
+  #echo "Process ID: $pid"
+  #echo "Command: $cmd"
+  #echo "Subprocesses:"
+  pstree -p $pid
 done | tr '\n' ';' | sed 's/;;/;/g'
