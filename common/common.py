dd6f63f4c76ad40e035a6614c5645939d94e24d8
lubusax <lu.bu.sax@gmail.com>
2023-10-22 15:24:14 +0200

    soso

--- common/common.py
+++ common/common.py
@@ -707,7 +707,7 @@ def get_network_info():
 
     network["eth0"]["mac_device"] = params.get("eth0_MAC_address") or False
     network["wlan0"]["mac_device"] = params.get("wlan0_MAC_address") or False
-    print("network calculated - "+network)
+    print("network calculated - "+str(network))
     return network
 
 
