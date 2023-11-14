c73b022a180357587f5bde23258e4bee1e475508
lubusax <lu.bu.sax@gmail.com>
2023-11-14 12:28:55 +0100

    [FIX] Bug non-utf8 char 0xc2 in logging setTimeZone

--- common/common_avoid_circularity.py
+++ common/common_avoid_circularity.py
@@ -2,7 +2,7 @@
 def insert_line_at_top(file_path, line_to_insert):
     try:
         # Read the existing file content
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
             lines = file.readlines()
 
         # Insert the new line at the top
@@ -13,7 +13,7 @@ def insert_line_at_top(file_path, line_to_insert):
             lines.pop()
 
         # Write the modified content back to the file
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
             file.writelines(lines)
 
         # print(f"Line inserted successfully in {file_path}")
@@ -21,6 +21,17 @@ def insert_line_at_top(file_path, line_to_insert):
     except FileNotFoundError:
         print(f"File not found: {file_path}")
         # Create the file if it doesn't exist
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
             file.write(line_to_insert)
             print(f"File {file_path} created with the inserted line")
+
+def clean_non_utf8(text):
+    cleaned_text = ""
+    for char in text:
+        try:
+            char.encode('utf-8')
+            cleaned_text += char
+        except UnicodeEncodeError:
+            cleaned_text += '?'  # Replace non-UTF-8 character with a placeholder
+
+    return cleaned_text
