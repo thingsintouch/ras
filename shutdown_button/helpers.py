c6c1c9130321fbf1f124926e1c7e4897fd1c2c09
lubusax <lu.bu.sax@gmail.com>
2023-12-08 15:44:27 +0100

    first

---
+++ shutdown_button/helpers.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+from common.constants import (
+    PIN_SHUTDOWN_BUTTON,
+    PIN_POWER_FOR_SHUTDOWN_BUTTON
+)
+
+# Define GPIO pins
+pin_shutdown_button = PIN_SHUTDOWN_BUTTON  
+pin_power_for_shutdown_button = PIN_POWER_FOR_SHUTDOWN_BUTTON
+
+def setup_GPIO_shutdown_button():
+    # Setup GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin_shutdown_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(pin_power_for_shutdown_button, GPIO.OUT)
+    # Provide power to the switch
+    GPIO.output(pin_power_for_shutdown_button, GPIO.HIGH)
+
+def is_shutdown_button_pressed():
+    input_state = GPIO.input(pin_shutdown_button)
+    if input_state == GPIO.LOW:
+        print("Button Pressed")
+        time.sleep(0.1)  # Add a small delay to debounce the button
+        return True
+    return False
+
+def cleanup_GPIO():
+    # Cleanup GPIO
+    GPIO.output(pin_power_for_shutdown_button, GPIO.LOW)  # Turn off power to the switch
+    GPIO.cleanup()
+
+def set_for_shutdown():
+    print("#"*100)
+    print("set for shutdown")
+    print("#"*100)
---
+++ shutdown_button/manager.py
@@ -0,0 +1,39 @@
+import time
+
+from common.constants import (
+    PERIOD_SHUTDOWN_BUTTON,
+    LOOPS_TO_WAIT_SHUTDOWN_BUTTON)
+from common.logger import loggerERROR
+from shutdown_button.helpers import (
+    setup_GPIO_shutdown_button,
+    is_shutdown_button_pressed,
+    set_for_shutdown,
+    cleanup_GPIO
+    )
+
+time.sleep(60)
+
+def main():
+    consecutive_press_count = 0
+    setup_GPIO_shutdown_button()
+
+    try:
+        while True:
+            if is_shutdown_button_pressed():
+                consecutive_press_count += 1
+            else:
+                consecutive_press_count = 0
+
+            if consecutive_press_count == LOOPS_TO_WAIT_SHUTDOWN_BUTTON:
+                set_for_shutdown()
+                break
+
+            time.sleep(PERIOD_SHUTDOWN_BUTTON)  
+
+    except Exception as e:
+        loggerERROR(f"exception on shutdown_button loop: {e}")
+    finally:
+        cleanup_GPIO()
+
+if __name__ == "__main__":
+    main()
