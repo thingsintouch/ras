0821119f3183320b37494a06b0b6b7a097a9067f
lubusax <lu.bu.sax@gmail.com>
2023-12-08 16:48:42 +0100

    no power

--- shutdown_button/helpers.py
+++ shutdown_button/helpers.py
@@ -12,23 +12,23 @@ pin_power_for_shutdown_button = PIN_POWER_FOR_SHUTDOWN_BUTTON
 def setup_GPIO_shutdown_button():
     # Setup GPIO
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin_shutdown_button, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
-    GPIO.setup(pin_power_for_shutdown_button, GPIO.OUT)
+    GPIO.setup(pin_shutdown_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    ### GPIO.setup(pin_power_for_shutdown_button, GPIO.OUT)
     # Provide power to the switch
-    GPIO.output(pin_power_for_shutdown_button, GPIO.HIGH)
+    ### GPIO.output(pin_power_for_shutdown_button, GPIO.HIGH)
 
 def is_shutdown_button_pressed():
     input_state = GPIO.input(pin_shutdown_button)
     print(input_state)
     if input_state == GPIO.LOW:
         print("Button Pressed")
-        time.sleep(0.1)  # Add a small delay to debounce the button
+        time.sleep(0.2)  # Add a small delay to debounce the button
         return True
     return False
 
 def cleanup_GPIO():
     # Cleanup GPIO
-    GPIO.output(pin_power_for_shutdown_button, GPIO.LOW)  # Turn off power to the switch
+    ### GPIO.output(pin_power_for_shutdown_button, GPIO.LOW)  # Turn off power to the switch
     GPIO.cleanup()
 
 def set_for_shutdown():
