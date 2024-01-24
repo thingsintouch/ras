82e489d0f62a335236f151b1ff303e956c389651
lubusax <lu.bu.sax@gmail.com>
2024-01-24 18:24:08 +0100

    au

--- bash_scripts/config/setup_aliases.sh
+++ bash_scripts/config/setup_aliases.sh
@@ -6,6 +6,7 @@ setup_aliases() {
     alias startras='bash $TASKS_DIR/startras.sh'
     alias stopras='bash $TASKS_DIR/stopras.sh'
     alias allalive='source $TASKS_DIR/all_alive_boolean.sh'
+    alias stats='source $TASKS_DIR/stats.sh'
 }
 # Call the function to set up aliases
 setup_aliases
--- bash_scripts/tasks/stats.sh
+++ bash_scripts/tasks/stats.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
-
 # Get the cpu load 1 min and 5 min from /proc/loadavg
-cpu_load_1min_percent=$(cat /proc/loadavg | awk '{print $1}')
-cpu_load_5min_percent=$(cat /proc/loadavg | awk '{print $2}')
+CPU_LOAD_1MIN_PERCENT=$(cat /proc/loadavg | awk '{print $1}')
+CPU_LOAD_5MIN_PERCENT=$(cat /proc/loadavg | awk '{print $2}')
 cpu_count=$(nproc)
 # Get the temperature from /sys/class/thermal/thermal_zone0/temp
 # Divide by 1000 to get degrees Celsius
-temp_C=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}')
-# Get the timestamp in PostgreSQL format
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+TEMP_C=$(cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}')
+# Get the TIMESTAMP in PostgreSQL format
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 # Get the RAM usage from free command
 # Divide by 1024 to get MB
-used_ram_MB=$(free -m | awk 'NR==2 {print $3}')
+USED_RAM_MB=$(free -m | awk 'NR==2 {print $3}')
 # Get the maximal RAM available in MB
-max_ram_MB=$(free -m | awk '/^Mem:/ {print $2}')
+MAX_RAM_MB=$(free -m | awk '/^Mem:/ {print $2}')
 # Get the percentage of RAM that is used
-# used_ram_percent=$(free -m | awk '/^Mem:/ {print $3/$2*100}')
+# USED_RAM_PERCENT=$(free -m | awk '/^Mem:/ {print $3/$2*100}')
 # Round the values to integers using printf
-max_ram_MB=$(printf "%.0f" $max_ram_MB)
-used_ram_MB=$(printf "%.0f" $used_ram_MB)
+MAX_RAM_MB=$(printf "%.0f" $MAX_RAM_MB)
+USED_RAM_MB=$(printf "%.0f" $USED_RAM_MB)
 # Round the values to integers using printf
-cpu_load_1min_percent=$(bc <<< "scale=0; ($cpu_load_1min_percent*100)/$cpu_count")
-cpu_load_5min_percent=$(bc <<< "scale=0; ($cpu_load_5min_percent*100)/$cpu_count")
-temp_C=$(printf "%.0f" $temp_C)
-used_ram_percent=$(bc <<< "scale=0; ($used_ram_MB*100)/$max_ram_MB")
+CPU_LOAD_1MIN_PERCENT=$(bc <<< "scale=0; ($CPU_LOAD_1MIN_PERCENT*100)/$cpu_count")
+CPU_LOAD_5MIN_PERCENT=$(bc <<< "scale=0; ($CPU_LOAD_5MIN_PERCENT*100)/$cpu_count")
+TEMP_C=$(printf "%.0f" $TEMP_C)
+USED_RAM_PERCENT=$(bc <<< "scale=0; ($USED_RAM_MB*100)/$MAX_RAM_MB")
 # Store and print the values in stats.txt
-echo "CPU load 1 min: $cpu_load_1min_percent%" 
-echo "CPU load 5 min: $cpu_load_5min_percent%" 
-echo "Temperature:    $temp_C°C" 
-echo "Timestamp:      $timestamp" 
-echo "used RAM:       $used_ram_MB MB"
-echo "max RAM:        $max_ram_MB MB"
-echo "used RAM:       $used_ram_percent%"
+echo "CPU load 1 min: $CPU_LOAD_1MIN_PERCENT%" 
+echo "CPU load 5 min: $CPU_LOAD_5MIN_PERCENT%" 
+echo "Temperature:    $TEMP_C°C" 
+echo "TIMESTAMP:      $TIMESTAMP" 
+echo "used RAM:       $USED_RAM_MB MB"
+echo "max RAM:        $MAX_RAM_MB MB"
+echo "used RAM:       $USED_RAM_PERCENT%"
+
+export $CPU_LOAD_1MIN_PERCENT
+export $CPU_LOAD_5MIN_PERCENT
+export $TEMP_C
+export $TIMESTAMP
+export $USED_RAM_MB
+export $MAX_RAM_MB
+export $USED_RAM_PERCENT
