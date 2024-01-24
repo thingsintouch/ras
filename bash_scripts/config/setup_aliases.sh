23bfb51fd6d0622e9c9f00e1bdac7269c4031810
lubusax <lu.bu.sax@gmail.com>
2024-01-24 17:50:44 +0100

    auto

--- bash_scripts/config/setup_aliases.sh
+++ bash_scripts/config/setup_aliases.sh
@@ -4,8 +4,8 @@
 setup_aliases() {
     alias startras='bash /home/pi/ras/bash_scripts/startras.sh'
     alias stopras='bash /home/pi/ras/bash_scripts/stopras.sh'
-    alias allalive='$SCRIPTS_DIR/tasks/all_alive_boolean.sh'
-
+    alias allalive='source $SCRIPTS_DIR/tasks/all_alive_boolean.sh'
+}
 # Call the function to set up aliases
 setup_aliases
 
