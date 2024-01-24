a2701b13e27ba6048826e26180d99aa26d88b4e2
lubusax <lu.bu.sax@gmail.com>
2024-01-24 17:43:50 +0100

    auto

--- bash_scripts/config/config_monitoring.sh
+++ bash_scripts/config/config_monitoring.sh
@@ -1,2 +1,5 @@
-source define_variables.sh
+# Get the directory of the current script
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/define_variables.sh"
 source $SCRIPTS_DIR/tasks/all_alive_boolean.sh
